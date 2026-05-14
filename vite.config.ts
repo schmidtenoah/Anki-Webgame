e461fb68e9407540cfaa9af75f26e9551dca41a0
schmidtenoah <schmidtenoah@gmail.com>
2026-05-14 22:16:13 +0200

    Deploy to GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 import { resolve } from "path";
 
 export default defineConfig({
+  base: "/Anki-Webgame/",
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
